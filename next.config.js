78832079642cb3a9d673e07abcbabfed02d332b3
fakeuserforall220-max <fakeuserforall220@gmail.com>
2026-04-10 17:50:20 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['acade-me.vercel.app', 'simats-seat-sync.vercel.app', 'expensa-five.vercel.app', 'safethelock.vercel.app', 'elvera-navy.vercel.app', 'go-rail-india.vercel.app', 'astra-self.vercel.app'],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+}
+
+module.exports = nextConfig
