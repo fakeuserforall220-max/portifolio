94736167ec3a347c24542865412d3e3ee5642971
fakeuserforall220-max <fakeuserforall220@gmail.com>
2026-04-10 17:53:29 +0530

    Create useScrollSpy.js

---
+++ src/hooks/useScrollSpy.js
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+export default function useScrollSpy(navItems) {
+  const [activeSection, setActiveSection] = useState(navItems[0].id)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = navItems.map(item => ({
+        id: item.id,
+        element: document.querySelector(item.href)
+      }))
+
+      const scrollPosition = window.scrollY + 150
+
+      for (const section of sections) {
+        if (section.element) {
+          const offsetTop = section.element.offsetTop
+          const offsetBottom = offsetTop + section.element.offsetHeight
+
+          if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+            setActiveSection(section.id)
+            break
+          }
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    handleScroll()
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [navItems])
+
+  return activeSection
+}
