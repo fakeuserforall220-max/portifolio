7bf0bc960516fc5b941461c174475f61efb3b8da
fakeuserforall220-max <fakeuserforall220@gmail.com>
2026-04-10 18:29:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,31 @@ const nextConfig = {
   images: {
     domains: ['acade-me.vercel.app', 'simats-seat-sync.vercel.app', 'expensa-five.vercel.app', 'safethelock.vercel.app', 'elvera-navy.vercel.app', 'go-rail-india.vercel.app', 'astra-self.vercel.app'],
   },
-  experimental: {
-    optimizeCss: true,
-  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
